5aae36e0c2b50fe22ef2a033098e025baeb531a1
Lazare Kvirtia <lazarekvirtia@gmail.com>
2021-12-31 12:13:20 +0400

    realtime database

--- app/src/main/java/com/lazo/firebase/MainActivity.java
+++ app/src/main/java/com/lazo/firebase/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -51,6 +52,11 @@ public class MainActivity extends AppCompatActivity {
 
         passwordChangeButton.setOnClickListener(view -> {
             startActivity(new Intent(MainActivity.this, PasswordChangeActivity.class));
+            finish();
+        });
+
+        realtimeDatabase.setOnClickListener(view -> {
+            startActivity(new Intent(MainActivity.this, RealtimeDatabaseActivity.class));
             finish();
         });
 
